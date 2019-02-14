eee06c35ccf6f93030fda5e652e7cf4d1ef31543
KDragon <elkarouani@gmail.com>
2019-02-14 15:19:33 +0100

    Task: adding model, dao packages and the index page to the project

---
+++ WebContent/WEB-INF/index.jsp
@@ -0,0 +1,21 @@
+<%@page import="Model.User"%>
+<%@page import="java.util.ArrayList"%>
+<%@ page language="java" contentType="text/html; charset=windows-1256"
+    pageEncoding="windows-1256"%>
+<% ArrayList<User> users = (ArrayList<User>) request.getAttribute("users"); %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+<title>Insert title here</title>
+</head>
+<body>
+	<form method="post" action="/GestionComptes/GererUsers">
+		cin <input type="text" name="cin" value="" /><br /><br />
+		nom <input type="text" name="nom" value="" /><br /><br />
+		prenom <input type="text" name="prenom" value="" /><br /><br />
+		adresse <input type="text" name="adresse" value="" /><br /><br />
+		<input type="submit" name="add" value="ajouter"><br /><br />
+	</form>
+</body>
+</html>
---
+++ src/dao/UserDAO.java
@@ -0,0 +1,76 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import Model.User;
+
+public class UserDAO {
+	
+	public static Connection getConnection(){
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/WebApp");
+			return ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static int addUser(User user){
+		Connection con = getConnection();
+		try {
+			return con.createStatement().executeUpdate(""
+					+ "insert into users values ('" + user.getCin()
+					+ "', '" + user.getNom()
+					+ "', '" + user.getPrenom()
+					+ "', '" + user.getAdresse()+"' )");
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+	
+	public static int modifyUser(User user){
+		Connection con = getConnection();
+		try {
+			return con.createStatement().executeUpdate(""
+					+ "update users SET " 
+					+ "`nom` = '" + user.getNom() + '"');
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+	
+	public static ArrayList<User> getAllUsers(){
+		Connection con = getConnection();
+		ArrayList<User> userList = new ArrayList<User>();
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM users ;");
+			while (rs.next()) {
+				String cin = rs.getString("cin");
+				String nom = rs.getString("nom");
+				String prenom = rs.getString("prenom");
+				String adresse = rs.getString("adresse");
+				
+				User user = new User(cin, nom, prenom, adresse);
+				
+				userList.add(user);
+			}
+			
+			return userList;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
